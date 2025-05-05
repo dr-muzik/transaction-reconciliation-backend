bea153a13d608c773d746fdb8538212689e9ecf5
Wisdom Peters <drmusicpiano@gmail.com>
2025-05-05 13:46:37 +0100

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin:
-      'http://localhost:5173, https://transaction-reconciliation-frontend.onrender.com, https://report-discrepancies.vercel.app', // allow your frontend
+  interface CorsOriginCallback {
+    (err: Error | null, allow?: boolean): void;
+  }
+
+  const corsOptions: CorsOptions = {
+    origin: (origin: string | undefined, callback: CorsOriginCallback) => {
+      const allowedOrigins: string[] = [
+        'http://localhost:5173',
+        'https://transaction-reconciliation-frontend.onrender.com',
+        'https://report-discrepancies.vercel.app',
+      ];
+
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,POST',
-    // credentials: true, // only if you're using cookies/auth
-  });
+  };
+
+  app.enableCors(corsOptions);
 
   await app.listen(process.env.PORT ?? 3000);
 }
